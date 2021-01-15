3c7a948caff1b26e91f63c48b1012060cfddecbb
Mötz Jensen <motz@e-s.dk>
2021-01-15 14:42:47 +0100

    Fix: Import of pester closer to the execution of pester test.

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,7 +1,6 @@
 $modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement")
 
 Install-Module "Pester" -MaximumVersion 4.99.99 -Force -SkipPublisherCheck -AllowClobber
-Import-Module "Pester" -MaximumVersion 4.99.99 -Force
 
 foreach ($module in $modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
